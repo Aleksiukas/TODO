ddb4c888cb04309ae831fac1c1dc24fdb249d678
Aleksiukas <edgaras.aleksiukas@gmail.com>
2020-05-14 10:37:27 +0300

    Render list #3

--- js/Todo.js
+++ js/Todo.js
@@ -1,28 +1,20 @@
 "use strict"
 
-let todo_list = [
-            {
-                description: 'pasigaminti kavos',
-                crated_on: '2020-05-05 15:01',
-                deadline: '2020-05-06 15:55',
-                status: 'todo'
-            },
-            {
-                description: 'nueit i bara',
-                crated_on: '2020-05-06 15:01',
-                deadline: '2020-05-07 15:55',
-                status: 'todo'
-            },
-            {
-                description: 'paguleti',
-                crated_on: '2020-07-05 15:01',
-                deadline: '2020-05-06 15:55',
-                status: 'todo'
-            }
-        ];
-        for (let i=0; i<todo_list.length; i++){
-        const todo = todo_list[i];
-        const sentence = `Darbas kuri reikia atlikti yra"${todo.description}" ir ji reikia atlikti iki '${todo.deadline}' siuo metu yra busenoje "${todo.status}".`;
-        console.log(sentence);
-        }
-    
+function renderList(list){
+    const listPlace = document.querySelector('.container');
+    let HTML = '';
+
+    for( let i=0; i<list.length; i++){
+    const todoItem = list[i];  
+    HTML+=`
+        <div class="item">
+            <div class="status ${todoItem.status}"></div>
+            <p class="description">${todoItem.description}</p>
+            <div class="deadline">${todoItem.deadline}</div>
+        </div>`;
+    }
+
+    return   listPlace.innerHTML += HTML;
+}
+
+renderList(todo_list);
